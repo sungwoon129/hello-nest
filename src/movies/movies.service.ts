beb3170d27d9d2690a6d65a47562e20e5eb3164a
master <sungwoony@kakao.com>
2023-12-04 03:17:41 +0900

    typeORM 0.3 트랜잭션

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -2,14 +2,16 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { Movie } from './entity/movie.entity';
 import { UpdateMovieDto } from './dto/update-movie.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { plainToInstance } from 'class-transformer';
 
 @Injectable()
 export class MoviesService {
   constructor(
     @InjectRepository(Movie)
     private readonly moviesRepository: Repository<Movie>,
+    private dataSource: DataSource,
   ) {}
 
   async getAll(): Promise<Movie[]> {
@@ -54,22 +56,63 @@ export class MoviesService {
     return await this.moviesRepository.save(movieData);
   }
 
-  // async createMany(movieList: Movie[]) {
-  //   const queryRunner = this.dataSource.createQueryRunner();
+  /**
+   * QueryRunner를 사용한 nestjs 트랜잭션 구현. QueryRunner는 dataSource나 Connection에 의존성을 가지고 있어 테스트하기 까다롭긴 하지만, 트랜잭션 전 과정을 개발자가 컨트롤 할 수 있어
+   * 공식문서에서 권장하는 방법.
+   * @param movieList
+   * @returns
+   */
+  async createManyByQueryRunner(
+    movieList: CreateMovieDto[],
+  ): Promise<number[]> {
+    // 컨트롤러에서 인스턴스화 시켰기 때문에 CreateMovieDto 내부 함수 사용 가능
+    movieList.forEach((t) => console.log(t.toEntity()));
 
-  //   await queryRunner.connect();
-  //   await queryRunner.startTransaction();
-  //   try {
-  //     await queryRunner.manager.save(movieList[0]);
-  //     await queryRunner.manager.save(movieList[1]);
+    const result = [];
+    const queryRunner = this.dataSource.createQueryRunner();
 
-  //     await queryRunner.commitTransaction();
-  //   } catch (err) {
-  //     // since we have errors lets rollback the changes we made
-  //     await queryRunner.rollbackTransaction();
-  //   } finally {
-  //     // you need to release a queryRunner which was manually instantiated
-  //     await queryRunner.release();
-  //   }
-  // }
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    try {
+      const entityArr: Movie[] = plainToInstance(Movie, movieList);
+      const savePromises: Promise<Movie>[] = entityArr.map((movie: Movie) =>
+        queryRunner.manager.save(movie),
+      );
+
+      // forEach와 같은 일반 반복문의 경우 비동기 함수의 처리에서 오류가 발생할 수 있으므로 Promise.all 사용 권장
+      await Promise.all(savePromises);
+      savePromises.forEach((promise) =>
+        promise.then((movie) => result.push(movie.id)),
+      );
+
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
+      return result;
+    }
+  }
+
+  /**
+   * DataSource 또는 EntityManager 사용한 트랜잭션 구현
+   */
+  public async createManyByEntityManager(
+    movieList: CreateMovieDto[],
+  ): Promise<number[]> {
+    const entityArr: Movie[] = plainToInstance(Movie, movieList);
+
+    //await this.dataSource.transaction(async (trasactionalEntityManager) => {...}) 도 동일하게 작용
+    const result = await this.dataSource.manager.transaction(
+      async (trasactionalEntityManager) => {
+        const data = await Promise.all(
+          entityArr.map((movie) =>
+            trasactionalEntityManager.getRepository(Movie).save(movie),
+          ),
+        );
+        return data.map((movie) => movie.id);
+      },
+    );
+    return result;
+  }
 }
